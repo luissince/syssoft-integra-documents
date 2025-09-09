dafee696c5aeb92dcb532dbdc83a1ae85ed49c3e
Luis Lara <xanderls25@icloud.com>
2025-09-09 15:53:01 -0500

    Actualización de creación de servicio de s3

--- src/helper/s3.helper.ts
+++ src/helper/s3.helper.ts
@@ -1,20 +1,14 @@
 import { S3Client, PutObjectCommand, GetObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import S3Singleton from "src/config/s3";
 
 /**
  * Sube un PDF a S3 y devuelve el "key" del archivo.
  * Ese key lo debes guardar en la base de datos.
  */
 export async function uploadPdfToS3(buffer: Buffer, key: string) {
+  const s3 = S3Singleton.getInstance();
   const s3BucketName = process.env.PDF_BUCKET;
-
-  const s3 = new S3Client({
-    region: process.env.AWS_REGION_S3,
-    credentials: {
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID_S3!,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY_S3!,
-    },
-  });
 
   const putCommand = new PutObjectCommand({
     Bucket: s3BucketName,
@@ -32,15 +26,8 @@ export async function uploadPdfToS3(buffer: Buffer, key: string) {
  * Genera un signed URL válido por 1 hora a partir del key guardado en la DB.
  */
 export async function getSignedUrlFromS3(key: string, expiresInSeconds = 3600) {
+  const s3 = S3Singleton.getInstance();
   const s3BucketName = process.env.PDF_BUCKET;
-
-  const s3 = new S3Client({
-    region: process.env.AWS_REGION_S3,
-    credentials: {
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID_S3!,
-      secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY_S3!,
-    },
-  });
 
   const getCommand = new GetObjectCommand({
     Bucket: s3BucketName,
